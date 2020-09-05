7ed078be451612331c32cd3d38de67501ea7e1fa
Shaymon Alvarez <shaymonalvarezz@gmail.com>
2020-09-05 20:12:56 -0300

    Exception handling - update

--- src/main/java/com/example/course/services/UserService.java
+++ src/main/java/com/example/course/services/UserService.java
@@ -3,6 +3,8 @@ package com.example.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -17,48 +19,48 @@ import com.example.course.services.exceptions.ResourceNotFoundException;
 //,Component ou o Service)
 @Service
 public class UserService {
-	
+
 	@Autowired
-	private UserRepository repository;//injeção de dependencia
-	
-	public List<User> findAll()
-	{
+	private UserRepository repository;// injeção de dependencia
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		//retorna exceção se não conseguir fazer o GET
-		return obj.orElseThrow(() ->new ResourceNotFoundException(id));
+		// retorna exceção se não conseguir fazer o GET
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
-	public User insert (User obj)
-	{
+
+	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
-	public void delete(Long id)
-	{
+
+	public void delete(Long id) {
 		try {
-		repository.deleteById(id);
-		}catch(EmptyResultDataAccessException e)
-		{
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(id);
-		}catch(DataIntegrityViolationException e)
-		{
+		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
 		}
 	}
-	
+
 	public User update (Long id, User obj)
 	{
-		User entity = repository.getOne(id);//prepara objeto monitorado, mas nao acessa banco de dados
-		updateData(entity,obj);
-		return repository.save(entity);
+		try
+		{
+			User entity = repository.getOne(id);//prepara objeto monitorado, mas nao acessa banco de dados
+			updateData(entity,obj);
+			return repository.save(entity);
+		}catch(EntityNotFoundException e)
+		{
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
-	private void updateData(User entity, User obj) 
-	{
+	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
